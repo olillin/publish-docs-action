d0edebabfee35f075a01c752224c310aa9984f08
Oli <oli@olillin.com>
2026-09-12 16:58:59 +0200

    Add JavaScript action template

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs.nixpkgs.url = "nixpkgs/nixpkgs-26.05";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.default = pkgs.mkShell {
+        nativeBuildInputs = [ pkgs.bashInteractive ];
+        buildInputs = with pkgs; [
+          nodejs_24
+          licensed
+        ];
+        shellHook = ''
+          export NIXSHELL="$NIXSHELL+publish-docs-action"
+        '';
+      };
+    });
+}
